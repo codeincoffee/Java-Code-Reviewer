9b72630fc2c39285abc204f03def00dabc985e99
Andy Chen <chenyulong1@gmail.com>
2011-04-10 09:16:39 +0000

    remove overload method getResult in BaseReview

--- src/com/codeincoffee/codereview/common/BaseReview.java
+++ src/com/codeincoffee/codereview/common/BaseReview.java
@@ -10,14 +10,11 @@ public class BaseReview {
 		return paths[paths.length-2] + FileUtil.DOT + paths[paths.length-1];
 	}
 
-	public Result getResult(ResultModule resultModule) {
-		if (resultModule.fail())
-			return Result.getFail(getName(), resultModule);
-		return Result.getSuccess(getName());
-	}
-	
 	public Result getResult(Module module, int result, int limit) {
-		return getResult(new ResultModule(module, result, limit));
+        ResultModule module = new ResultModule(module, result, limit);
+        if (resultModule.fail())
+		    return Result.getFail(getName(), resultModule);
+		return Result.getSuccess(getName());
 	}
 
 	
